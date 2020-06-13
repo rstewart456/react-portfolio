33701199f48c2adf0da068e121e80139c51b8719
Ronald Stewart <rstewart456@gmail.com>
2020-06-12 21:07:43 -0400

    Updates

---
+++ precache-manifest.67b81d85c8c5895565499813e2cdbaaa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c9f4c34defd6cc5f1eb59a9118d2f4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e902d9fd2c859eb3a202",
+    "url": "/react-portfolio/static/css/main.4f8d39cc.chunk.css"
+  },
+  {
+    "revision": "3009fd1493281e789909",
+    "url": "/react-portfolio/static/js/2.71f12e88.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/react-portfolio/static/js/2.71f12e88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e902d9fd2c859eb3a202",
+    "url": "/react-portfolio/static/js/main.12f81526.chunk.js"
+  },
+  {
+    "revision": "bee14ebf850387594d39",
+    "url": "/react-portfolio/static/js/runtime-main.8c64cdeb.js"
+  },
+  {
+    "revision": "107a63dd16dd438fff2db259f93a7d75",
+    "url": "/react-portfolio/static/media/codeproject.107a63dd.jpg"
+  },
+  {
+    "revision": "1cb86b023f301261e84d96de70496c95",
+    "url": "/react-portfolio/static/media/coding-bg.1cb86b02.png"
+  }
+]);
